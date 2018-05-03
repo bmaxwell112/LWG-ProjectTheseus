f4bb6d461c8c5a2cafb8858944d28271cb592fd6
Cody <lordxiek@gmail.com>
2018-05-02 22:00:17 -0400

    Turret code started

--- Assets/TurretAI.cs
+++ Assets/TurretAI.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class TurretAI : MonoBehaviour {
 
+    Transform player;
+    [SerializeField] Transform firingArc;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    private void DefineRotation()
+    {
+        Vector3 diff = player.transform.position - transform.position;
+        diff.Normalize();
+
+        firingArc.eulerAngles = MovementFunctions.LookAt2D(transform, diff.x, diff.y);
+    }
 }
