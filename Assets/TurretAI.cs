8fd5166f9729a935b0daafb7798a1e894dd7196a
Cody <lordxiek@gmail.com>
2018-05-02 23:07:37 -0400

    Updated all enemy spawners

--- Assets/TurretAI.cs
+++ Assets/TurretAI.cs
@@ -6,15 +6,21 @@ public class TurretAI : MonoBehaviour {
 
     Transform player;
     [SerializeField] Transform firingArc;
+    private RobotLoadout rolo;
+
+    bool firing, localCooldown;
 
 	// Use this for initialization
 	void Start () {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        rolo = GetComponent<RobotLoadout>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+        DefineRotation();
+        RangedAttack();
 	}
 
     private void DefineRotation()
@@ -23,5 +29,33 @@ public class TurretAI : MonoBehaviour {
         diff.Normalize();
 
         firingArc.eulerAngles = MovementFunctions.LookAt2D(transform, diff.x, diff.y);
+    }
+
+    public void RangedAttack()
+    {
+        //print("Ranged Attack");
+        if (!localCooldown)
+        {
+            //print("Cool down false");
+            StartCoroutine(SpawnBullets(rolo.loadout[3]));
+        }
+    }
+
+    IEnumerator SpawnBullets(Item weapon)
+    {
+        //print("Start Co-routine");
+        RangedWeapon rw = Database.instance.ItemsRangedWeapon(weapon);
+        localCooldown = true;
+        firing = true;
+        while (firing)
+        {
+            GameObject bullet = Instantiate(Resources.Load("bullet", typeof(GameObject))) as GameObject;
+            bullet.GetComponent<BulletWeapon>().BulletSetup(rw, transform.position, firingArc, "Player", gameObject.tag);
+            yield return new WaitForSeconds(rw.rangeWeaponRateOfFire);
+            yield return new WaitForSeconds(0.5f);
+            firing = false;
+        }
+        //print("End Co-routine");
+        localCooldown = false;
     }
 }
