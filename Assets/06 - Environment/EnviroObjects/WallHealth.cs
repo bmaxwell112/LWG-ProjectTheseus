bc7ea2927622882d9253c4be402f51a1e1898877
Cody <lordxiek@gmail.com>
2018-05-02 21:03:34 -0400

    Added code to internal room objects

---
+++ Assets/06 - Environment/EnviroObjects/WallHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallHealth : MonoBehaviour {
+
+    private RobotLoadout rolo;
+    private SpriteRenderer spRender;
+
+	// Use this for initialization
+	void Start () {
+        rolo = GetComponent<RobotLoadout>();
+        spRender = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(rolo.hitPoints <= 50)
+        {
+            spRender.color = new Color(0, 0, 0);
+
+            if (collision.gameObject.tag == "FriendlyBullets")
+            {
+                Physics2D.IgnoreCollision(collision.collider, GetComponent<Collider2D>());
+            }
+        }
+    }
+}
