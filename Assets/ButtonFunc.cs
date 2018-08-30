365f00516d8234402a79acd28f52133ad4b778e3
Cody <lordxiek@gmail.com>
2018-08-29 23:34:28 -0400

    Busted but, as close as it has been

---
+++ Assets/ButtonFunc.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonFunc : MonoBehaviour
+{
+
+    Text infoText;
+
+    // Use this for initialization
+    void Start()
+    {
+        ReplaceText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ReplaceText()
+    {
+        infoText = FindObjectOfType<infodummy>().GetComponent<Text>();
+        infoText.text = QuestController.currentQuest.eventDesc;
+    }
+
+    public void ActivateButton()
+    {
+        ReplaceText();
+        QuestController.currentQuest = GetComponent<ButtonSetup>().refEvent;
+        print("Active quest is now " + GetComponent<ButtonSetup>().refEvent.eventName);
+        print("Check via QC - " + QuestController.currentQuest.eventName);
+    }
+}
