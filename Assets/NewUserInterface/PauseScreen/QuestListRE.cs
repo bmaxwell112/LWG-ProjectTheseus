80d58aa2bc8de72ea23a78e8a9d9f9bf88eb7120
Cody <lordxiek@gmail.com>
2018-11-28 19:19:42 -0500

    Fixed buttons spawning inappropriately

--- Assets/NewUserInterface/PauseScreen/QuestListRE.cs
+++ Assets/NewUserInterface/PauseScreen/QuestListRE.cs
@@ -7,32 +7,37 @@ public class QuestListRE : MonoBehaviour {
 	[SerializeField] ButtonSetup questButton;
 	RectTransform rt;
 
-
-	public void LoadActiveQuests()
+    public void LoadActiveQuests()
 	{
-		// Find old buttons
-		rt = GetComponent<RectTransform>();
-		ButtonSetup[] oldBtn = GetComponentsInChildren<ButtonSetup>();
-		foreach (ButtonSetup btn in oldBtn)
-			Destroy(btn.gameObject);
-
+        // Find old buttons
+        rt = GetComponent<RectTransform>();
+        ButtonSetup[] oldBtn = GetComponentsInChildren<ButtonSetup>();
+        foreach (ButtonSetup btn in oldBtn)
+        {
+            Destroy(btn.gameObject);
+        }
+			
 		// Go through active events
 		bool display = false;
 		int i = 0;
-		foreach (QuestEvent quest in QuestController.activeEvents)
-		{
-			// Spawn button and pass the quest data
-			ButtonSetup newBtn = Instantiate(questButton, transform) as ButtonSetup;			
-			newBtn.SetupQuestButton(quest);
-			i++;
-			// Make first item spawned display on the panel
-			//if (!display)
-			//{
-			//	print(quest.eventName);
-			//	newBtn.ActivateButton();
-			//	display = true;
-			//}
-		}
+        foreach (QuestEvent quest in QuestController.activeEvents)
+		    {
+                if (quest.active == true)
+                {
+
+                    // Spawn button and pass the quest data
+                    ButtonSetup newBtn = Instantiate(questButton, transform) as ButtonSetup;
+                    newBtn.SetupQuestButton(quest);
+                    i++;
+                    // Make first item spawned display on the panel
+                    //if (!display)
+                    //{
+                    //	print(quest.eventName);
+                    //	newBtn.ActivateButton();
+                    //	display = true;
+                    //}
+                }
+            }
 		rt.sizeDelta = new Vector2(0, 150 * i);
-	}
+    }
 }
