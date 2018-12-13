738c17b2d345e926eee634ade3c772380135a3fd
Cody <lordxiek@gmail.com>
2018-12-12 23:16:16 -0500

    Created boss objects

---
+++ Assets/DisableGravity.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableGravity : MonoBehaviour {
+
+    private Rigidbody2D rBody;
+
+	// Use this for initialization
+	void Start () {
+        rBody = GetComponent<Rigidbody2D>();
+        rBody.gravityScale = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
